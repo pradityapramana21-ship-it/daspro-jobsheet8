34a66ad8e77f52516838548423b0a84ac2525072
radit <pradityapramana21@gmail.com>
2025-11-14 14:50:04 +0700

    4=2

--- NilaiKelompok23.java
+++ NilaiKelompok23.java
@@ -4,18 +4,27 @@ public class NilaiKelompok23 {
     Scanner sc = new Scanner(System.in);
     int i = 1, nilai;
     int totalNilai, rataNilai;
-        while (i <= 6) {
-            System.out.println("Kelompok " + i);
 
-    totalNilai = 0;
-    for (int j = 1; j <= 5; j++) {
-        System.out.print(" Nilai dari kelompok Penilai " + j + ":");
-        nilai = sc.nextInt();
-}
-    rataNilai = totalNilai / 5;
-    i++;
+    int maxRata = -1; 
+    int kelompokMax = -1;  
 
-      }
-
-  }  
+    while (i <= 6) {
+    System.out.println("Kelompok " + i);
+        totalNilai = 0;
+        for (int j = 1; j <= 5; j++) {
+            System.out.print(" Nilai dari kelompok Penilai " + j + ": ");
+            nilai = sc.nextInt();
+            totalNilai = totalNilai + nilai;
+        }
+        rataNilai = totalNilai / 5;
+        System.out.println("Rata-rata nilai = " + rataNilai + "\n");
+        if (rataNilai > maxRata) {
+            maxRata = rataNilai;
+            kelompokMax = i;
+        }
+        i++;
+    }
+    System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok "
+                        + kelompokMax + " dengan rata-rata " + maxRata);
+  }
 }
