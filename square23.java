077e4e6e7175a0a9c851d44ec96cd44ce8ca0880
radit <pradityapramana21@gmail.com>
2025-11-14 13:49:30 +0700

    2

---
+++ square23.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class square23 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukan nilai n = ");
+    int n = sc.nextInt();
+    for (int iOuter = 1; iOuter <= n; iOuter++) {
+    for (int i = 1; i <= n; i++) {
+        System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
