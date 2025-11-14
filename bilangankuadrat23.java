819eb7642206f6562222e7030e4fb247531d521c
radit <pradityapramana21@gmail.com>
2025-11-14 14:59:01 +0700

    5

---
+++ bilangankuadrat23.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ public class bilangankuadrat23 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int jumlah = 0;
+            System.out.print("n = " + i + " → jumlah kuadrat = ");
+        for (int j = 1; j <= i; j++) {
+                int kuadrat = j * j;
+                jumlah += kuadrat;
+            System.out.print(kuadrat);
+                if (j < i) {
+            System.out.print(" + ");
+                }
+            }
+            System.out.println(" = " + jumlah);
+        }
+    }
+ }
+ 
+    
+
